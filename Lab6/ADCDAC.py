4a4e3b274ea5931082485b6c85785e6588f09fcb
patrickscholl <patrick.scholl@berkeley.edu>
2018-03-07 14:53:00 -0800

    Signed-off-by: patrickscholl <patrick.scholl@berkeley.edu>

--- Lab6/ADCDAC.py
+++ Lab6/ADCDAC.py
@@ -1,2 +1,14 @@
 from machine import Pin, ADC, DAC
-import board import DAC1
+from board import DAC1, ADC6
+from time import sleep
+
+
+dac1 = DAC(Pin(DAC1))
+adc = ADC(Pin(ADC6))
+adc.attn(ADC.ATTN_11DB)
+for i in range(255):
+	val = dac1.write(i)
+	print(val)
+	delay(.1)
+	val2 = adc.read()
+	print(val2)
