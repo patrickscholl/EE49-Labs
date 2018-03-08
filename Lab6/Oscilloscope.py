214fc7f5bcac8c303fdc2373c9c2172cc27e8dd4
patrickscholl <patrick.scholl@berkeley.edu>
2018-03-07 23:39:11 -0800

    Signed-off-by: patrickscholl <patrick.scholl@berkeley.edu>

--- Lab6/Oscilloscope.py
+++ Lab6/Oscilloscope.py
@@ -1,2 +1,10 @@
-from machine import pwm
-from board import 
+from machine import Pin, PWM
+from board import A5, A6
+
+p1 = Pin(A5, mode =Pin.OPEN_DRAIN)
+p2 = Pin(A6, mode =Pin.OPEN_DRAIN)
+
+
+PWM(p1, freq=5000, duty = 20, 0)
+
+PWM(p2, freq=8000, duty = 60, 0)
